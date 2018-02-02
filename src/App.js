d35f43414a48b61bf77b76c01ed3c33230f72fce
Rob <robstlezin@gmail.com>
2018-02-02 13:41:40 -0700

    imported first into App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { First } from './components/component.first';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        'hi'
+      <First></First>
       </div>
     );
   }
