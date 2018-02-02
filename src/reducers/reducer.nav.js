67aa06542570fce09c71ad8892aacc09a872ce99
Rob <robstlezin@gmail.com>
2018-02-02 16:07:54 -0700

    syntax error in reducer

--- src/reducers/reducer.nav.js
+++ src/reducers/reducer.nav.js
@@ -8,18 +8,18 @@ export default function navReducer (state = initialState, action) {
 
   switch(action.type){
     case 'FIRST-OPTION':
-      return
-        {...state,
+      return {
+          ...state,
           navChoice: 'FIRST-OPTION'
         }
     case 'SECOND-OPTION':
-      return
-        {...state,
+      return{
+          ...state,
           navChoice: 'SECOND-OPTION'
         }
     case 'THIRD-OPTION':
-      return
-        {...state,
+      return{
+          ...state,
           navChoice: 'THIRD-OPTION'
         }
   }
