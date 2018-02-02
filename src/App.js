412005480090e2380704aa07fcc2eb2ff93c422f
Rob <robstlezin@gmail.com>
2018-02-02 15:44:03 -0700

    added horizontal to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Nav } from './components/component.nav'
+import { Horizontal } from './components/component.horiz'
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Horizontal></Horizontal>
         <Nav></Nav>
       </div>
     );
